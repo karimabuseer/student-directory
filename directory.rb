b3ca5fda3cfb90a710271219d05b140464ef3893
Karim Abu-Seer <karimabuseer@gmail.com>
2021-06-17 09:13:07 +0000

    Exercises 1-11 completed

--- directory.rb
+++ directory.rb
@@ -1,12 +1,23 @@
 # Creating student array
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  months = ["january", "febuary", "march", "april", "may", "june", "july",
+  "august", "september", "october", "november", "december"]
+  puts "Please enter the names of the students one by one"
+  puts "To finish, just hit return twice when asked for name"
   students = []
-  name = gets.chomp
+  name = gets.delete("\n")
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "What cohort are they in? If you want the default cohort, hit return"
+    coh = 'placehold'
+    until months.include?(coh) || coh.empty? do
+      coh = gets.chomp
+      if coh.strip.empty? && !months.include?(coh)
+        students << {name: name, cohort: :november, birth_country: :England}
+      else
+        students << {name: name, cohort: coh.to_sym, birth_country: :England}
+      end
+    end
+    puts students.length < 2 ? "Now we have #{students.count} student" : "Now we have #{students.count} students"
     name = gets.chomp
   end
   students
@@ -20,7 +31,21 @@ end
 
 # Method to list names
 def print(names)
-  names.each_with_index { |x, i| puts "#{i+1}. #{x[:name]} (#{x[:cohort]} cohort)" } 
+  i = 0
+  while i < names.length
+    puts "#{i+1}. #{names[i][:name]} (#{names[i][:cohort]} cohort)".center(30)
+    i += 1
+  end  
+end
+
+# Method to print list of students grouped by Cohort
+def cohort_print
+  cohortlist = []
+  students.each { |stu| cohortlist << x[cohort] }
+  cohortlist.each { |coh| 
+    puts coh
+    z = students.collect{|p| p[:name] if p[:cohort] == coh}.compact
+    puts z }
 end
 
 # Method to print names that begin with a certain letter
